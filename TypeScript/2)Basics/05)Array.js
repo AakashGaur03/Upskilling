eabeebf756302b88034de616244b58854864177d
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-09-09 17:03:27 +0530

    Array

---
+++ TypeScript/2)Basics/05)Array.js
@@ -0,0 +1,8 @@
+var superHeroes = [];
+var heroPwer = [];
+var arr = [];
+var allUsers = [];
+superHeroes.push("spidy");
+heroPwer.push(2);
+// allUsers.push("")  // Gives Error
+allUsers.push({ name: "Spiderman", isActive: true });
---
+++ TypeScript/2)Basics/05)Array.ts
@@ -0,0 +1,25 @@
+const superHeroes: string[] = [];
+const heroPwer: number[] = [];
+const arr: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+// Array of Array
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [1, 2, 3],
+];
+
+superHeroes.push("spidy");
+heroPwer.push(2);
+
+// allUsers.push("")  // Gives Error
+
+allUsers.push({ name: "Spiderman", isActive: true });
+
+export {};
