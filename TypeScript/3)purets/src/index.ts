7639d36c48d41806eae41e9750efc72f731bafe5
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-09-10 12:07:47 +0530

    Getter, setter and private method

--- TypeScript/3)purets/src/index.ts
+++ TypeScript/3)purets/src/index.ts
@@ -13,12 +13,31 @@
 
 // we can write class in TS like this also we need to write readonly seprately rest we can write inside of constructor
 class User {
+  private _courseCount = 1;
   private readonly country: string = "India";
   constructor(
     public email: string,
     public name: string,
     private city: string = ""
   ) {}
+
+  private deleteToken() {
+    console.log("Token Deleted");
+  }
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course Count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
+  //   TS setter dont have any return type thats how TS is designed
 }
 
 const dave = new User("dave@gmail.com", "dave");
@@ -32,3 +51,5 @@ const dave = new User("dave@gmail.com", "dave");
 // we can also use # instead of private
 // But Note # is present in JS and TS both
 // private is only in TS
+
+// dave.deleteToken()
