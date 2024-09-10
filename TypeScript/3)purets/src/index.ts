f22886cf7f89f075b6846314b91343d029327afe
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-09-10 11:49:41 +0530

    Classes

--- TypeScript/3)purets/src/index.ts
+++ TypeScript/3)purets/src/index.ts
@@ -1,2 +1,16 @@
-console.log("Hello Ji Ok ")
-console.log("Hello Ji Ok ")
+// console.log("Hello Ji Ok ")
+// console.log("Hello Ji Ok ")
+class User {
+  email: string;
+  name: string;
+  city: string = "";
+  readonly country: string = "India";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const dave = new User("dave@gmail.com", "dave");
+dave.city = "Delhi";
+// dave.country = "USA";
