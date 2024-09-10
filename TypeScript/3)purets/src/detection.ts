99214b9d7a3ba8386a9e252fed1e779fc4686a75
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-09-10 17:15:25 +0530

    instanceof & Type Predicates

--- TypeScript/3)purets/src/detection.ts
+++ TypeScript/3)purets/src/detection.ts
@@ -29,3 +29,33 @@ function isAdminAccount(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+// JavaScript has an operator for checking whether or not a value is an “instance” of another value
+// Anything that uses new kewword or have potential of having it. then we can use instanceof
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// Type Predicates
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fist Food";
+  } else {
+    pet;
+    return "Bird Food";
+  }
+}
