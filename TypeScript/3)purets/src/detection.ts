86b02166e29a47177d197d9ebde9609e57da6346
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-09-11 10:20:46 +0530

    Discriminated Union & Exhaustiveness checking

--- TypeScript/3)purets/src/detection.ts
+++ TypeScript/3)purets/src/detection.ts
@@ -59,3 +59,44 @@ function getFood(pet: Fish | Bird) {
     return "Bird Food";
   }
 }
+
+// Discriminated Unions
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+interface Square {
+  kind: "square";
+  side: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else if (shape.kind === "square") {
+    return shape.side * shape.side;
+  }
+  return shape.length * shape.width;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
+  }
+}
